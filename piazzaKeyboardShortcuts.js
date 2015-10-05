5754373b2789571fe3d98a8c5773cd980d0883e2
aldilaff <aldilaff@gmail.com>
2015-10-05 18:23:15 -0500

    fixed #1

--- piazzaKeyboardShortcuts.js
+++ piazzaKeyboardShortcuts.js
@@ -16,7 +16,13 @@ key('k', function(){
 	var $next = $( "#question_feed_questions .feed_item.selected" ).next();
 	if( $next.length==0){
 		// alert("last element!"); //alert
-		$('.selected').closest(".question_group").next().find(".feed_item").first().click();
+		var $next_question_group = $('.selected').closest(".question_group").next();
+		//help from http://stackoverflow.com/questions/20600083/jquery-how-to-find-if-element-has-inline-property-not-in-css
+		while($next_question_group.find('.item-list-in-bucket').css('display')=='none'){
+			// alert("next question_Group is hidden"); //alert
+			$next_question_group = $next_question_group.next();
+		}
+			$next_question_group.find(".feed_item").first().click();
 	}else{
 		$next.click();
 	} 
@@ -27,7 +33,13 @@ key('j', function(){
 	var $prev = $( "#question_feed_questions .feed_item.selected" ).prev();
 	if( $prev.length == 0){
 		// alert("first element!"); //alert
-		$('.selected').closest(".question_group").prev().find(".feed_item").last().click();
+		var $prev_question_group = $('.selected').closest(".question_group").prev();
+		
+		while($prev_question_group.find('.item-list-in-bucket').css('display')=='none'){
+			// alert("prev question_Group is hidden"); //alert
+			$prev_question_group = $prev_question_group.prev();
+		}
+			$prev_question_group.find(".feed_item").last().click();;
 	}else{
 		$( "#question_feed_questions .feed_item.selected" ).prev().click(); 
 
